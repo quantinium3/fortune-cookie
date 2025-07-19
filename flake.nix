7ae7dbbff17fa799690f162d4f4fea6f44b39688
quantinium03 <quant@quantinium.dev>
2025-07-19 21:32:06 +0530

    we using proper hashes

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
@@ -17,13 +17,12 @@
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
           pname = "fortune-cookie-${version}";
           version = "0.1.0";
-          src = pkgs.lib.sourceByRegex ./. [
-            "Cargo\.lock"
-            "Cargo\.toml"
-            "src"
-            "src/bin"
-            ".*\.rs$"
-          ];
+          src = pkgs.fetchFromGitHub {
+            owner = "quantinium3";
+            repo = "fortune-cookie";
+            rev = "1518c312a13dc6593666b2d38c01f24e8719b2e6"; # replace with the rev we got earlier
+            sha256 = "sha256-ehkM8XvKHysYVaH5xdleVsatCtSfUrWxOC/9ADakGbg="; # replace with the hash we got earlier
+          };
           cargoLock = { lockFile = ./Cargo.lock; };
           buildInputs = with pkgs; [ openssl ];
 
