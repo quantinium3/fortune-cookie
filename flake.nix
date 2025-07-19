1518c312a13dc6593666b2d38c01f24e8719b2e6
quantinium03 <quant@quantinium.dev>
2025-07-19 21:02:09 +0530

    pinned to stable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
